36da9aee458e960bd13afeaa2da48bf722304a38
danielruss <druss@mail.nih.gov>
2022-04-12 15:45:20 -0400

    added CSS

--- bigstudy.js
+++ bigstudy.js
@@ -1,26 +1,29 @@
 import { transform } from 'https://episphere.github.io/quest/replace2.js';
-import { rbAndCbClick } from "https://episphere.github.io/quest/questionnaire.js";
 
-console.log("... in bigstudy.js")
+// Clear the old Responses when the clear response button is clicked
+document.getElementById("clearResp").addEventListener("click", () => {
+    localforage.clear()
+    location.reload()
+})
 
-
-// callback function on submit..
-function dataSubmitted(obj) {
-    document.getElementById("resultsDiv").innerText = obj.toString()
-}
-
-
+// we dont actually have a backend, so for testing
+// I'm using this FakeDB object as a mock object for the 
+// database.
 let FakeDB = {}
+
+// storeResponse take the object from quest and 
+// adds all the elements to the 
 function storeResponse(obj) {
     console.log("store response ", obj)
     for (const prop in obj) {
         console.log(`setting db[${prop}]=${obj[prop]} `)
         FakeDB[prop] = obj[prop];
     }
+    getResponse()
 }
 
 function getResponse() {
-    const tableElement = document.getElementById("respTable")
+    const tableElement = document.getElementById("respTableBody")
     let makeTableData = (data) => {
         let td = document.createElement("td")
         td.innerText = data
@@ -33,16 +36,18 @@ function getResponse() {
         table.appendChild(tr)
     }
 
-    document.getElementById("respTable").innerText = ""
+    document.getElementById("respTableBody").innerText = ""
     for (const prop in FakeDB) {
         makeTableRow(tableElement, prop, FakeDB[prop])
     }
-    document.getElementById("resultsDiv").appendChild(tableElement)
 
     return FakeDB
 }
 
-// add the survey
+/*
+ This is the line that calls quest.  The example from our paper is
+ place in the questDiv element.
+*/
 transform.render({
     url: "https://danielruss.github.io/questionnaire/paper_example1.txt",
     activate: true,
@@ -50,4 +55,8 @@ transform.render({
     retrieve: getResponse
 }, "questDiv")
 
+
+/* Let the developer window have access to the FakeDB and 
+the getResponse function */
 window.FakeDB = FakeDB
+window.getResponse = getResponse
