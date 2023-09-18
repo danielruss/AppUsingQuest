d577ef55315a9f3dd5f8d71f90a06644dd04b1a5
Jonas Almeida <jalmeida@mathbiol.org>
2023-09-18 17:10:51 -0400

    sync working

--- syncDonate.mjs
+++ syncDonate.mjs
@@ -8,22 +8,27 @@ function getToken(){ // extract parameters from search or hash
     let donateToken=null
     if(location.href.match(/donateToken=[^&#]+/)){
         donateToken=(location.search+location.hash).match(/donateToken=[^&#]+/)[0].split('=')[1]
-        //location.href=location.href.replace(/[#&]donateToken=[^&#]+/,'')
+        //remove token from hash if that is where it is (that would be ideal):
+        location.hash=location.hash.replace(/[#&]donateToken=[^&#]+/,'')
     }
     return donateToken
 }
 
-let token = getToken()
+let token = await getToken()
 
-async function sync(dt,token=token,url="https://donate.jonasalmeida.repl.co"){ // synch data to donate backend
-    await dona.setUrl(url)
+// test tokens at test deployment: https://replit.com/@jonasalmeida/donate#data/tokens.txt
+async function sync(dt,token,url="https://donate.jonasalmeida.repl.co"){ // synch data to donate backend
+    dona.setUrl(url) // set deployment
     console.log('deployment:',dona)
-    let old = JSON.parse(await dona.get(url))
+    let old = JSON.parse(await dona.get(token))
     console.log('old data:',old)
-    return await dona.post(token,dt)
+    let res = dona.post(token,dt)
+    res.then(console.log)
+    return old
 }
 
 export{
+    dona,
     token,
     sync
 }
